e002b368144ddc907e6d2b92ddc69731dc8c632b
Cray <cray@cray.com>
2019-02-21 20:36:35 +0100

    java bez skryptu

--- generatorC.sh
+++ generatorC.sh
@@ -4,6 +4,7 @@ echo "#include <stdio.h>" >> $1
 echo "int main ()" >> $1
 echo "{" >> $1
 echo '	printf("Hello, '$2'!\n");' >> $1
+# mozna zapisac echo "  printf(\"Hello, $2!\n\");" >> $1
 echo "	return 0;" >> $1
 echo "}" >> $1
 gcc $1
