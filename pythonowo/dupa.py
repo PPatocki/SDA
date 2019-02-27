f0c36915d8d7fbc68404e0fe8ed0d1af11fee320
Cray <cray@cray.com>
2019-02-27 20:50:00 +0100

    Pierwsze zajecia python

---
+++ pythonowo/dupa.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3.7
+
+#a='Piotr'
+#print(f"Czesc {a}")
+
+#print('Podaj imie ziomus')
+#imie = input()
+#print('Siema', imie)
+#print(f'siema {imie}')
+
+#from datetime import datetime
+#rok = datetime.now().year
+#print('Podaj swoj wiek')
+#wiek = int(input())
+#print(f'Masz {wiek} lat. Urodziles sie w {rok-wiek} roku')
+
+pizza=40
+beer=30
+sum=beer+pizza
+rate=0.08
+total=sum+(sum*rate)
+print(total)
