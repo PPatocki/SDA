9fa64cd1158f750c40b1bd0ad281b39b9024baad
Cray <cray@cray.com>
2019-04-09 21:06:29 +0200

    flask i docker

---
+++ PiotrBrzozowski/flaskowo/pathvariable.py
@@ -0,0 +1,8 @@
+from flask import abort
+
+@app.route('/tobo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_tasks(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
