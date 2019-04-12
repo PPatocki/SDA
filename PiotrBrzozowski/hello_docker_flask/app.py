54646afaf85081a7e349279b93d05bc82a3cf1c0
Cray <cray@cray.com>
2019-04-12 20:06:18 +0200

    final z Piotrkiem

--- PiotrBrzozowski/hello_docker_flask/app.py
+++ PiotrBrzozowski/hello_docker_flask/app.py
@@ -4,8 +4,8 @@ from flask import Flask
 app = Flask(__name__) 
 @app.route('/')
 def index():
-    return "Flask example docker!"
+    return "Hello Piotr Patocki"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
 
